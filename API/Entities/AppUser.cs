8e334b405e76fe72c92f97dd00c701879d925891
Kenneth Cheung <pushup365.io@gmail.com>
2021-01-16 18:34:14 +0100

    API skeleton

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        // ASP.NET Core Identity 
+        public string UserName { get; set; }
+    }
+}
